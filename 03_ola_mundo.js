6e0f90eabf60ca5c69fb4a9524001260e027c522
Helton Heleno <helton_heleno@hotmail.com>
2023-07-04 15:02:22 -0300

    first commit

---
+++ 03_ola_mundo.js
@@ -0,0 +1,2 @@
+//Console log é um comando para exibir na tela
+console.log("Olá mundo")
---
+++ 04_valores_e_variaveis.js
@@ -0,0 +1,24 @@
+let nome = "Guilherme"
+
+const idade = 20
+
+idade = 10
+
+console.log(nome)
+console.log(idade)
+
+const endereco = "Avenida 1"
+
+let precoDoLeite = 9.5
+let precoDoQueijo = 30 
+let precoDaPizza
+
+precoDaPizza = 16
+console.log("Preço do leite:" , precoDoLeite)
+console.log("Preço queijo:" , precoDoQueijo)
+console.log("Preço da Pizza:", precoDaPizza) 
+
+precoDoLeite = 8
+
+console.log(precoDoLeite)
+console.log(precoDoQueijo)
